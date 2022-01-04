7e5f51c197457548cf4aad2a11e702555f0120b2
saravana46 <saravana8ganesan@gmail.com>
2022-01-04 14:21:02 +0530

    First commit

---
+++ Pods/Target Support Files/Pods-vipertest-vipertestUITests/Pods-vipertest-vipertestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vipertest_vipertestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_vipertest_vipertestUITests
+@end
---
+++ Pods/Target Support Files/Pods-vipertest/Pods-vipertest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vipertest : NSObject
+@end
+@implementation PodsDummy_Pods_vipertest
+@end
